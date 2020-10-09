eda6b6b5dd56e394503f2cbaf1d450f1f36fbf95
99002600 <72514189+99002600@users.noreply.github.com>
2020-10-09 17:09:59 +0530

    Add files via upload

---
+++ railway/inc/railways.h
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+void create();
+int cancel(int);
+void display();
---
+++ railway/test/test.c
@@ -0,0 +1,13 @@
+#include "unity.h"
+#include "program_choose.h"
+
+//Required by the test unity framework
+void setUp() {}
+
+//Required by the unity framework
+void tearDown() {}
+
+void testing_prime_number(void)
+{
+    TEST_ASSERT_EQUAL(1, cancel(1));
+}
